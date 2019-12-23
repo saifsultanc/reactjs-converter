60283ade153dd45512e355ef51ffd229ff1cd4f9
Saif <saifraikkonen@gmail.com>
2019-12-23 14:16:26 +0530

    setting up project and building components

---
+++ src/components/InputField.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { TextField, InputAdornment, Grid } from "@material-ui/core";
+
+const InputFields = ({ data, handleInput }) => {
+  return (
+    <Grid item xs={6}>
+      <TextField
+        variant="outlined"
+        label={data.label}
+        value={data.currentValue}
+        name={data.name}
+        onChange={handleInput}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">{data.input}</InputAdornment>
+          )
+        }}
+        style={{ width: "100%" }}
+      />
+    </Grid>
+  );
+};
+
+export default InputFields;
---
+++ src/components/Selector.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import {
+  Grid,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem
+} from "@material-ui/core";
+
+const Selector = ({ data, handleSelector }) => {
+  return (
+    <Grid item xs={data.size}>
+      <FormControl variant="outlined" style={{ width: "100%" }}>
+        <InputLabel>{data.label}</InputLabel>
+        <Select
+          value={data.selectedValue}
+          onChange={handleSelector}
+          inputProps={{ name: data.populateType }}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {data.populateType === "mainSelector"
+            ? data.populateWith.map((ele, index) => (
+                <MenuItem value={data.measurements[index]} key={index}>
+                  {ele}
+                </MenuItem>
+              ))
+            : data.populateWith.map(ele => (
+                <MenuItem value={ele.abbr} key={ele.abbr}>
+                  {eye.plural}
+                </MenuItem>
+              ))}
+        </Select>
+      </FormControl>
+    </Grid>
+  );
+};
+
+export default Selector;
